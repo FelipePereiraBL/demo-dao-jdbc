92a7b9cd2bc533f9fee656aaa1a71c3629f030cb
FelipePereiraBL <felipereirabl@gmail.com>
2021-07-05 16:22:48 -0300

    consert update implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -17,6 +17,7 @@ public class Seller implements Serializable
 	public Seller()
 	{
 		
+		
 	}
 	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) 
 	{
