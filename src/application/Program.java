dc4075a87aa3d88c38bc656676dc30ee1d5b15c1
FelipePereiraBL <felipereirabl@gmail.com>
2021-07-05 15:58:52 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -25,12 +26,25 @@ public class Program {
 			System.out.println(obj);
 		}
 	    
-	    System.out.println("\n===Teste 2: Seller findAll===");
+	    System.out.println("\n===Teste 3: Seller findAll===");
 	    list=sellerDao.findAll();
 	    for (Seller obj : list) 
 	    {
 			System.out.println(obj);
 		}
+	    
+	    System.out.println("\n=== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n===TESTE 5: seller update===");
+		seller=sellerDao.findById(1);
+		System.out.println("Antes do update: "+seller);
+		
+		seller.setName("Martha");
+		sellerDao.update(seller);
+		System.out.println("Update concluido! "+seller);
 
 	}
 
